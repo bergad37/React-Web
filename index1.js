18651dc9879eb9050b7002307c265ad4de564015
bergad37 <bergad37@gmail.com>
2023-05-11 15:05:34 +0200

    Initial commit

---
+++ index1.js
@@ -0,0 +1,14 @@
+
+
+const navBar=(<nav>
+<h1>My Brand Website</h1>
+<ul>
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contacts</li>
+</ul>
+</nav>)
+
+
+ReactDOM.render(navBar,document.getElementById("root"));
+
